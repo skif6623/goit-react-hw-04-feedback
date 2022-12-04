615cbfdf75a11784cb7f4bc27e3841fe54d9b941
Roman Tunkiv <85829645+skif6623@users.noreply.github.com>
2022-12-04 18:14:36 +0200

    created Notification component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import { FeedbackOptions } from './FeedbackOptions/FeedbackOptions';
 import { Statistics } from './Statistics/Statistics';
+import { Section } from './Section/Section';
+import { Notification } from './Notification/Notification';
 export class App extends Component {
   state = {
     good: 0,
@@ -37,8 +39,7 @@ export class App extends Component {
       this.countPositiveFeedbackPercentage().toFixed(0);
 
     return (
-      <div>
-        <h1>Please leave feedback</h1>
+      <Section title="Please leave feedback">
         <FeedbackOptions onLeaveFeedback={this.addFeedback}>
           good
         </FeedbackOptions>
@@ -48,14 +49,18 @@ export class App extends Component {
         <FeedbackOptions onLeaveFeedback={this.addFeedback}>
           bad
         </FeedbackOptions>
-        <Statistics
-          good={good}
-          neutral={neutral}
-          bad={bad}
-          total={totalFeedback}
-          positivePercentage={positivePercentage}
-        />
-      </div>
+        {totalFeedback === 0 ? (
+          <Notification message="There is no feedback" />
+        ) : (
+          <Statistics
+            good={good}
+            neutral={neutral}
+            bad={bad}
+            total={totalFeedback}
+            positivePercentage={positivePercentage}
+          />
+        )}
+      </Section>
     );
   }
 }
