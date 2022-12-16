42f456703d8db2fc5635ffbbbc12c029dc65988b
Roman Tunkiv <85829645+skif6623@users.noreply.github.com>
2022-12-16 16:50:03 +0200

    added use Reducer

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,62 @@
-import { useState } from 'react';
+// Реалізація через State------------------------------------------------------------------------------------------------------------------------------------------------------
+
+// import { useState } from 'react';
+// import { FeedbackOptions } from './FeedbackOptions/FeedbackOptions';
+// import { Statistics } from './Statistics/Statistics';
+// import { Section } from './Section/Section';
+// import { Notification } from './Notification/Notification';
+// import { GlobalStyle } from './GlobalStyle';
+// import { Box } from './Box';
+
+// export function App() {
+//   const [good, setGood] = useState(0);
+//   const [neutral, setNeutral] = useState(0);
+//   const [bad, setBad] = useState(0);
+
+//   const countTotalFeedback = () => {
+//     return good + bad + neutral;
+//   };
+
+//   const countPositiveFeedbackPercentage = () => {
+//     return (good / countTotalFeedback()) * 100;
+//   };
+
+//   return (
+//     <>
+//       <Section title="Please leave feedback">
+//         <Box display="flex" justifyContent="center" gridGap={20} mb={20}>
+//           <FeedbackOptions onLeaveFeedback={() => setGood(state => state + 1)}>
+//             good
+//           </FeedbackOptions>
+//           <FeedbackOptions
+//             onLeaveFeedback={() => setNeutral(state => state + 1)}
+//           >
+//             neutral
+//           </FeedbackOptions>
+//           <FeedbackOptions onLeaveFeedback={() => setBad(state => state + 1)}>
+//             bad
+//           </FeedbackOptions>
+//         </Box>
+//         {countTotalFeedback() === 0 ? (
+//           <Notification message="There is no feedback" />
+//         ) : (
+//           <Statistics
+//             good={good}
+//             neutral={neutral}
+//             bad={bad}
+//             total={countTotalFeedback()}
+//             positivePercentage={countPositiveFeedbackPercentage().toFixed(0)}
+//           />
+//         )}
+//       </Section>
+//       <GlobalStyle />
+//     </>
+//   );
+// }
+
+// Реалізація через useReducer--------------------------------------------------------------------------------------------------------------------------------------------------
+
+import { useReducer } from 'react';
 import { FeedbackOptions } from './FeedbackOptions/FeedbackOptions';
 import { Statistics } from './Statistics/Statistics';
 import { Section } from './Section/Section';
@@ -6,32 +64,53 @@ import { Notification } from './Notification/Notification';
 import { GlobalStyle } from './GlobalStyle';
 import { Box } from './Box';
 
+function leaveFeedback(state, actions) {
+  switch (actions.type) {
+    case 'good':
+      return { ...state, good: state.good + actions.payload };
+    case 'neutral':
+      return { ...state, neutral: state.neutral + actions.payload };
+    case 'bad':
+      return { ...state, bad: state.bad + actions.payload };
+
+    default:
+      return state;
+  }
+}
+
 export function App() {
-  const [good, setGood] = useState(0);
-  const [neutral, setNeutral] = useState(0);
-  const [bad, setBad] = useState(0);
+  const [state, dispatch] = useReducer(leaveFeedback, {
+    good: 0,
+    bad: 0,
+    neutral: 0,
+  });
 
   const countTotalFeedback = () => {
+    const { good, bad, neutral } = state;
     return good + bad + neutral;
   };
 
   const countPositiveFeedbackPercentage = () => {
-    return (good / countTotalFeedback()) * 100;
+    return (state.good / countTotalFeedback()) * 100;
   };
 
   return (
     <>
       <Section title="Please leave feedback">
         <Box display="flex" justifyContent="center" gridGap={20} mb={20}>
-          <FeedbackOptions onLeaveFeedback={() => setGood(state => state + 1)}>
+          <FeedbackOptions
+            onLeaveFeedback={() => dispatch({ type: 'good', payload: 1 })}
+          >
             good
           </FeedbackOptions>
           <FeedbackOptions
-            onLeaveFeedback={() => setNeutral(state => state + 1)}
+            onLeaveFeedback={() => dispatch({ type: 'neutral', payload: 1 })}
           >
             neutral
           </FeedbackOptions>
-          <FeedbackOptions onLeaveFeedback={() => setBad(state => state + 1)}>
+          <FeedbackOptions
+            onLeaveFeedback={() => dispatch({ type: 'bad', payload: 1 })}
+          >
             bad
           </FeedbackOptions>
         </Box>
@@ -39,9 +118,9 @@ export function App() {
           <Notification message="There is no feedback" />
         ) : (
           <Statistics
-            good={good}
-            neutral={neutral}
-            bad={bad}
+            good={state.good}
+            neutral={state.neutral}
+            bad={state.bad}
             total={countTotalFeedback()}
             positivePercentage={countPositiveFeedbackPercentage().toFixed(0)}
           />
@@ -51,70 +130,3 @@ export function App() {
     </>
   );
 }
-
-// export class App extends Component {
-//   state = {
-//     good: 0,
-//     neutral: 0,
-//     bad: 0,
-//   };
-
-//   addFeedback = e => {
-//     const { name } = e.currentTarget;
-
-//     this.setState(prevState => {
-//       return {
-//         [name]: prevState[name] + 1,
-//       };
-//     });
-//   };
-
-//   countTotalFeedback = () => {
-//     const { good, bad, neutral } = this.state;
-
-//     return good + bad + neutral;
-//   };
-
-//   countPositiveFeedbackPercentage = () => {
-//     const { good } = this.state;
-
-//     return (good / this.countTotalFeedback()) * 100;
-//   };
-
-//   render() {
-//     const { good, neutral, bad } = this.state;
-//     const totalFeedback = this.countTotalFeedback();
-//     const positivePercentage =
-//       this.countPositiveFeedbackPercentage().toFixed(0);
-
-//     return (
-//       <>
-//         <Section title="Please leave feedback">
-//           <Box display="flex" justifyContent="center" gridGap={20} mb={20}>
-//             <FeedbackOptions onLeaveFeedback={this.addFeedback}>
-//               good
-//             </FeedbackOptions>
-//             <FeedbackOptions onLeaveFeedback={this.addFeedback}>
-//               neutral
-//             </FeedbackOptions>
-//             <FeedbackOptions onLeaveFeedback={this.addFeedback}>
-//               bad
-//             </FeedbackOptions>
-//           </Box>
-//           {totalFeedback === 0 ? (
-//             <Notification message="There is no feedback" />
-//           ) : (
-//             <Statistics
-//               good={good}
-//               neutral={neutral}
-//               bad={bad}
-//               total={totalFeedback}
-//               positivePercentage={positivePercentage}
-//             />
-//           )}
-//         </Section>
-//         <GlobalStyle />
-//       </>
-//     );
-//   }
-// }
